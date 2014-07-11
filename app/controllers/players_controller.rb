78b3ba70f5aafc48ef63584e819c50a5fab0aa01
albertbahia <albert.bahia1@gmail.com>
2014-07-10 23:15:04 -0400

    Added create feature for Player and added error messages to display on screen for forms.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,9 +5,16 @@ class PlayersController < ApplicationController
   end
 
   def new
+    @player = Player.new
   end
 
   def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to player_path(@player)
+    else
+      render :new
+    end
   end
 
   def show
